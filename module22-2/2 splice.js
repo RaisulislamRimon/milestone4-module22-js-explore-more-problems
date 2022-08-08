57043baef816105a06c6c005135d53d8b620ecd0
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-08 07:46:37 +0000

    update

--- module22-2/2 splice.js
+++ module22-2/2 splice.js
@@ -1,6 +1,4 @@
-const friends = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-];
+const friends = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(friends.length);
 
 console.log(friends.slice(0, 3));
@@ -8,3 +6,8 @@ console.log(friends.length);
 
 console.log(friends.splice(3, 6));
 console.log(friends.length);
+
+const friends1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// console.log(friends1.splice(3, 2));
+const friendsSplice = friends1.splice(3, 2, 11, 12);
+console.log(friendsSplice);
